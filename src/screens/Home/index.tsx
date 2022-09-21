22bb427178592899b839a4dfcf89cbfe66f49259
Jaqueline <piresjaqueline17@gmail.com>
2022-09-21 20:40:51 -0300

    Component Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,20 @@
-import { Container } from './styles';
-
+import { Percent } from '../../components/Percent';
+import { Avatar, Container, HomeHeader, Logo, Texto } from './styles';
+import logoImg from '../../assets/Logo.png';
+import avatarImg from '../../assets/avatar.png';
+import { ButtonAddMeal } from '../../components/ButtonAddMeal';
 
 export function Home() {
   return (
     <Container>
-    
+        <HomeHeader>
+        <Logo source={logoImg} />
+        <Avatar source={avatarImg} />
+      </HomeHeader>
+
+      <Percent />
+      <ButtonAddMeal />
+
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,34 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
 export const Container = styled.View`
+  flex: 1;
+  padding: 24px;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+`;
+export const HomeHeader = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 36px;
+`;
 
+export const Logo = styled.Image``;
+
+export const Avatar = styled.Image`
+  width: 40px;
+  height: 40px;
+  border-radius: 999px;
+  border-width: 2px;
+  border-color: ${({ theme }) => theme.COLORS.GRAY_600};
+`;
+
+export const Title = styled.Text`
+  color: #FFF;
+  font-size: 32px;
+`;
+
+export const Texto = styled.Text`
+  color: #000;
+  font-size: 18px;
 `;
