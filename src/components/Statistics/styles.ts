e2d41d2722c04bfe926f779d9f31ae0ec4c0c6f1
Jaqueline <piresjaqueline17@gmail.com>
2022-09-21 20:41:51 -0300

    Component Statistics

---
+++ src/components/Statistics/styles.ts
@@ -0,0 +1,66 @@
+import styled, { css } from 'styled-components/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { ArrowLeft } from 'phosphor-react-native'
+
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.GREEN_LIGHT};
+`
+
+export const StatisticsHeader = styled.View`
+  width: 100%;
+  align-items: center;
+  padding: 32px 24px;
+`
+
+export const StatisticsBackButton = styled.TouchableOpacity`
+  align-self: flex-start;
+`
+
+export const StatisticsBackIcon = styled(ArrowLeft).attrs(({ theme }) => {
+  return {
+    size: 32,
+    color: theme.COLORS.GREEN_DARK
+  }
+})``
+
+export const StatisticsHeaderTitle = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_700};
+    font-size: ${theme.FONT_SIZE.XXL}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `};
+
+  margin-top: -32px;
+`
+
+export const StatisticsHeaderSubtitle = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_600};
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `};
+`
+export const StatisticsContainer = styled.View`
+  align-items: center;
+  
+`
+export const StatisticsContent = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+
+`
+
+export const StatisticsContentTitle = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  color: ${({ theme }) => theme.COLORS.GRAY_700};
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  margin-top: 33px;
+`
+
+export const MealsInDietColumContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+`
