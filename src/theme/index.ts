76c592d884db50ef11c8b3ff677d3f2fd7062427
Jaqueline <piresjaqueline17@gmail.com>
2022-09-21 20:43:00 -0300

    Defining the application theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -23,8 +23,11 @@ export default {
     BOLD: 'Roboto_700Bold'
   },
   FONT_SIZE: {
-    SM: 12,
-    MD: 14,
-    LG: 16,
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 32,
   }
 };
