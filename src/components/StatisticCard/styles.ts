bc717648484c4c52068dd643ae274767a10bb430
Jaqueline <piresjaqueline17@gmail.com>
2022-09-21 20:42:01 -0300

    Component StatisticsCard

---
+++ src/components/StatisticCard/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native'
+
+export const StatisticCardContainer = styled.View`
+
+`
+
+
+export const StatisticCardNumber = styled.Text`
+  align-self: center;
+`
+
+export const StatisticCardDescription = styled.Text``
