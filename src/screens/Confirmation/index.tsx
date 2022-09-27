78c98f0895e87160fe0fd502c8b7e4e91b3e0002
Jaqueline <piresjaqueline17@gmail.com>
2022-09-27 20:25:15 -0300

    feat: Visual create to Confirmation

---
+++ src/screens/Confirmation/index.tsx
@@ -0,0 +1,27 @@
+import { useNavigation } from '@react-navigation/native';
+import { ButtonGreen } from '../../components/ButtonGreen';
+import { Container, Content, Emoji, Title, Footer, Subtitle} from './styles';
+
+
+export function Confirmation() {7
+  const navigation = useNavigation();
+
+  function handleHome() {
+    navigation.navigate('home');
+  }
+  return (
+    <Container>
+      <Content>
+        <Emoji>😊</Emoji>
+        <Title>Prontinho</Title>
+        <Subtitle>
+          Agora vamos começar a cuidar das suas 
+          saúde com muito cuidado.
+        </Subtitle>
+        <Footer>
+          <ButtonGreen title="Começar" onPress={handleHome}/>
+        </Footer>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/screens/Confirmation/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: space-around;
+`;
+export const Content = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 30px;
+`;
+export const Emoji = styled.Text`
+  font-size: 78px;
+`;
+export const Title = styled.Text`
+  font-size: 22px;
+  text-align: center;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  line-height: 38px;
+  margin-top: 15px;
+`;
+export const Subtitle = styled.Text`
+  font-size: 17px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  font-size: 17px;
+  text-align: center;
+  padding: 10px 0;
+  color: ${({ theme }) => theme.COLORS.height};
+
+`;
+export const Footer = styled.View`
+  width: 100%;
+  padding: 0 50px;
+  margin-top: 20px;
+`;
